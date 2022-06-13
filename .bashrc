05ee70b6fd97c9df1b22272673d1bcc33ecf9f8f
Keith Adams <kma@slack-corp.com>
2022-06-13 18:21:07 +0000

    Purge some fbisms.

--- .bashrc
+++ .bashrc
@@ -9,11 +9,6 @@ if [ -f /etc/bashrc ]; then
 	. /etc/bashrc
 fi
 
-# Source Facebook definitions
-if [ -f /home/engshare/admin/scripts/master.bashrc ]; then
-	. /home/engshare/admin/scripts/master.bashrc
-fi
-
 [ -f ${HOME}/.sh/fb.sh ] && . ${HOME}/.sh/fb.sh
 
 if [ $(uname -s) = "Linux" ]; then
--- .sh/fb.sh
+++ .sh/fb.sh
@@ -9,7 +9,6 @@ export FBID=530929372
 if [ -f /home/engshare/svnroot/admin/scripts/master.bashrc ]; then
   . /home/engshare/svnroot/admin/scripts/master.bashrc
 fi
-unalias ls
 alias ls="ls -F"
 
 function srcgrep {
@@ -62,49 +61,29 @@ wc_nsleaf03=search.snc1.nsleaf.people03
 # Linux. And C++.
 export GLIBCXX_FORCE_NEW=1
 
-# WWW build support
-export USER_ROOT=/data/users/$USER
-export HPHP_FACEBOOK_WWW=$USER_ROOT/www
-export WWW=$USER_ROOT/www
-
 function setprompt() {
-  d=$(basename $HPHP_HOME)
+  d=$(basename $WS_HOME)
   PS1="($HOSTNAME:$d:$HPBLD:\W)\$ "
   export PS1
 }
 
-function chp() {
-  POSSIBLE_HPHP_HOME=$(echo $1 | sed -e's/:/ /' | awk '{print $1}')
-  POSSIBLE_HPBLD=$(echo mumble$1 | sed -e's/:/ /' | awk '{print $2}')
-  if [ -d "$POSSIBLE_HPHP_HOME" ]; then
-    export HPHP_HOME=$POSSIBLE_HPHP_HOME
-    export TREE=$POSSIBLE_HPHP_HOME
+function cws() {
+  POSSIBLE_WS_HOME=$(echo $1 | sed -e's/:/ /' | awk '{print $1}')
+  if [ -d "$POSSIBLE_WS_HOME" ]; then
+    export WS_HOME=$POSSIBLE_WS_HOME
+    export TREE=$POSSIBLE_WS_HOME
   fi
-  echo " --=-- $POSSIBLE_HPBLD --=--"
-  [ "$POSSIBLE_HPBLD" = "opt" -o "$POSSIBLE_HPBLD" = "dbg" ] &&
-    export HPBLD=$POSSIBLE_HPBLD
-  export HPHP_LIB=$HPHP_HOME/bin
   setprompt
 }
 
-function cdhp() {
-  cd $HPHP_HOME/src
+function cdws() {
+  cd $WS_HOME/src
 }
-chp $HOME/local-hphp:dbg
-
-function hpenv() {
-  env | grep HPHP_
-}
-
-function vqj() {
-  hpmk verify_quick_jit
-}
+cws /workspace
 
 # PPROF tools prefix
 #PATH=/mnt/gvfs/third-party/bd3267234489a2fa6a5cc35642dde2633e11504c/gcc-4.6.2-glibc-2.13/binutils/binutils-2.21.1/bin/:$PATH
 #PATH=/mnt/gvfs/third-party/f7e1450772192c96981844338f4517baf835ebf1/gcc-4.6.2-glibc-2.13/binutils/binutils-2.21.1/4bc2c16/bin/:$PATH
-HPHPERR=/var/facebook/logs/hphp/error.log
-export HPBLD=dbg
 
 # Messes up default behavior of tc-print. Eh.
 #export HHVM_REPO_CENTRAL_PATH=/data/users/kma/central.hhbc
